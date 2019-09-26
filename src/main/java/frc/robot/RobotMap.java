4e01cd5422b8d06767685a1d04f6c67121bc8498
MattP34 <mattthewpropp@westmister.net>
2019-09-25 20:22:37 -0400

    updated RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,10 +1,8 @@
 package frc.robot;
 
 public class RobotMap {
-    public final static int FRONT_LEFT_TALON = 0;
-    public final static int FRONT_RIGHT_TALON = 1;
-    public final static int BACK_LEFT_TALON = 2;
-    public final static int BACK_RIGHT_TALON = 3;
+    public final static int LEFT_TALON = 0;
+    public final static int RIGHT_TALON = 1;
 
     public final static int PCM_ID = 20; //Compressor
 
